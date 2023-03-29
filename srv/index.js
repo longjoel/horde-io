e90a753fc5434d78d180338e0c12afe0dea91233
Joel Longanecker <joel.longanecker@gmail.com>
2023-03-29 14:40:11 -0500

    added webpack

--- srv/index.js
+++ srv/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use('/',express.static(path.join(__dirname,'..','horde-io-client','dist')));
 
 app.listen(3000);
